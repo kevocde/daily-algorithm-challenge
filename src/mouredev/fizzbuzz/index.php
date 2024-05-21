3bc9847b89f55b1b21ddca8d8f5f61f605ed3b74
kevocde <kevocde@gmail.com>
2024-05-21 07:37:30 -0500

    added: comment explanation of the solution

--- src/mouredev/fizzbuzz/index.php
+++ src/mouredev/fizzbuzz/index.php
@@ -8,6 +8,19 @@
  *  - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
 
+/**
+ * Print the sequence of number from 1 to 100 (include 1 and 100),
+ * 
+ * How it works?
+ *  Iterate in I from 1 until 100,
+ *  for each iteration check if the I number is multiple of 3 and and save the value
+ *  in a variable, create a variable type string with the output value, and check if
+ *  is multiple of each number, add the string to the output variable, finally check
+ *  if the output variable is empty (the number is not multiple of any number),
+ *  add the I value to the string and print the output variable.
+ *
+ * @return void
+ */
 function fizzBuzz() {
   for ($i = 1; $i <= 100; $i++) {
     $output = "";
